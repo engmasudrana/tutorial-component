ed5a20f1c1820bf1e374764ed1fbd15c8be3fc25
Masud Rana <masudranawd2019@gmail.com>
2021-10-22 17:13:51 -0700

    text editor

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TextEditor from "./components/TextEditor/TextEditor";
+
+const App = () => {
+
+    return (
+        <>
+            <TextEditor/>
+        </>
+    );
+};
+
+export default App;
